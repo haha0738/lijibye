cf15b63b0a3ea5eafd451463bbc1a92993244148
Dermot Chiou <dermot@ifalo.com.tw>
2017-10-22 14:08:07 +0800

    first commit

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <router-view></router-view>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="index">
+        <h1>栗雞掰</h1>
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3">
+                <div class="thumbnail">
+                    <img src="https://graph.facebook.com/100006626567064/picture?type=square&width=500&height=500" class="img-circle" width="200" height="200">
+                    <div class="caption">
+                        <h3>栗子</h3>
+                        <p>2016 - 2017</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <img src="https://graph.facebook.com/100002465127567/picture?type=square&width=500&height=500" class="img-circle" width="200" height="200">
+                    <div class="caption">
+                        <h3>雞雞</h3>
+                        <p>2016 - 2017</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    #index{
+        text-align:center;
+        margin-top: 60px;
+    }
+
+    img{
+        filter: grayscale(100%);
+    }
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
